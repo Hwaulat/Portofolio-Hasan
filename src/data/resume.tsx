ba3da178c4f9079b6b1cf0dff01b66c67804c5e2
Hasan <waulathan@gmail.com>
2024-09-23 05:33:39 +0700

    first

---
+++ src/data/resume.tsx
@@ -0,0 +1,345 @@
+import { Icons } from "@/components/icons";
+import { CodeIcon, HomeIcon, NotebookIcon, PencilLine } from "lucide-react";
+
+export const DATA = {
+  name: "Hasan Waulat",
+  initials: "HW",
+  url: "https://dillion.io",
+  location: "",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description: "UI/UX Research and Design from Ambon",
+  summary:
+  "Lulusan Teknik Informatika dari Universitas Muhammadiyah Malang, dengan pengalaman 2 tahun sebagai UI/UX Designer. Terbiasa bekerja di bidang desain antarmuka dan pengalaman pengguna (UI/UX), dengan portofolio yang mencakup berbagai proyek. Sebelumnya, telah menjalani beberapa magang, termasuk 6 bulan terakhir di PT Ragdalion Revolusi Industri sebagai UI/UX Designer, sesuai dengan minat karir jangka panjang. Saat ini, terbuka untuk peluang karir di bidang UI/UX Design dan siap berkembang lebih lanjut dalam industri.",
+  avatarUrl: "/me.png",
+  skills: [
+    "UI Design",
+    "UX Design",
+    "UX Reserach",
+    "Wireframe",
+    "User Research",
+    "Usability Testing",
+    "Behavioural Design",
+    "Design Systems",
+    "Responsive Design",
+
+  ],
+  softskills: [
+    "Growth Midset",
+    "Self-Regulated Learning",
+    "Learning Drive",
+    "Critical Thinking",
+    "Problem Solving",
+    "Creative Thinking",
+    "Learning Agility",
+  ],
+  tools: ["Figma", "Jira", "Trello", "Maze"],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: CodeIcon, label: "Projects" },
+    // { href: "#", icon: CodeIcon, label: "Projects" },
+    // { href: "#", icon: PencilLine, label: "Notes" },
+  ],
+  contact: {
+    email: "hasanjobs3@gmail.com",
+    tel: "+6285254308418",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://dub.sh/dillion-github",
+        icon: Icons.github,
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/hasan-waulat/",
+        icon: Icons.linkedin,
+        navbar: true,
+      },
+      Instagram: {
+        name: "Instagram",
+        url: "https://www.instagram.com/hanwaulat?igsh=MXVuc2pkOTFoaHB0bw==",
+        icon: Icons.instagram,
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://dub.sh/dillion-youtube",
+        icon: Icons.youtube,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "PT Ragdalion Revolusi Industri",
+      href: "https://ragdalion.com/",
+      badges: [],
+      location: "Onsite",
+      title: "UI/UX Designer (Onsite)",
+      logoUrl: "/Ragdalion.png",
+      start: "Mei",
+      end: "November 2024",
+      description:
+      // "dsds",
+      <ul>
+        <li>• Membuat design concept Corrective Action Reports untuk client MAYORA sebanyak 12 page dengan menggunakan Figma.</li>
+        <li>• Membuat design project Maintenance Management System berbasis Mobile dan Website untuk client TRICENTRUM FORTUNA dengan menggunakan Figma.</li>
+        <li>• Melakukan convert design project Mobile Andon Packout menjadi Web Based dan responsive untuk client MATTEL dengan menggunakan Figma.</li>
+        <li>• Membuat design concept MRP Software sebanyak 10 fitur dari data diagram MRP yang diberikan client INOAC dengan menggunakan Figma.</li>
+        <li>• Membuat design project QC System dari data form excel 13 customer untuk client RESONAC dengan menggunakan Figma.</li>
+        <li>• Membuat design project QC Incoming Dashboard Web Admin berdasarkan data real yang sudah ada untuk client API (Auto Plastic Indonesia) dengan menggunakan Figma.</li>
+        <li>• Melakukan redesign terhadap tampilan dashboard MaintenancePro Mobile dengan menggunakan Figma.</li>
+        <li>• Membuat design concept berbasis (Andon,Web Admin,Tab) E-Logistic berdasarkan alur proses real untuk client INOAC dengan menggunakan Figma.</li>
+        <li>• Membuat design concept web admin Task Management berdasarkan proses real yang terjadi di lapangan untuk Customer HITACHI ASTEMO dengan menggunakan Figma.</li>
+        <li>• Membuat Design Project Maintenance Management System web admin dan mobile app fitur checklist untuk customer SHARP. </li>
+        <li>• Membuat Design Project Maintenance Managament System web admin dan mobile app keseluruhan fitur (Checklist, Preventive, Corrective) untuk customer Sinar Meadow</li>
+      </ul>
+    },
+    {
+      company: "Azura Labs",
+      href: "https://azuralabs.id/",
+      badges: [],
+      location: "Remote",
+      title: "UI/UX Designer (Remote)",
+      logoUrl: "/Azura.jpeg",
+      start: "Mei ",
+      end: "Juli 2024",
+      description:
+      <ul>
+        <li>• Membuat design mobile app tracking produk pertalite dan bio solar untuk client Pertamina Patra Logistic dengan menggunakan Figma.</li>
+        <li>• Membuat design dashboard dan transaksi agar user dapat mengirim reservasi produk untuk client Pertamina Patra Logistic dengan menggunakan Figma.</li>
+        <li>• Membuat design dashboard berdasarkan data real excel untuk client Alinemar dengan menggunakan Figma.</li>
+        <li>• Membuat design concept 5 fitur berbasis website untuk client shizouka dengan menggunakan Figma.</li>
+      </ul>
+    },
+    {
+      company: "Digimarly",
+      badges: [],
+      href: "https://digimarly.com/",
+      location: "Remote",
+      title: "UI/UX Designer (Remote)",
+      logoUrl: "/digimarly.jpeg",
+      start: "April ",
+      end: "November 2024",
+      description:
+      <ul>
+        <li>• Melakukan redesign landing page Digimarly “landingpage.digimarly.com” bersama rekan tim yang sudah berhasil di deploy dengan menggunakan Figma.</li>
+        <li>• Membuat design website Ranisha Beauty bersama tim untuk meningkatkan omset dan mitra dengan menggunakan figma yang sudah berhasil di deploy “ranishabeauty.com” dengan menggunakan Figma.</li>
+        <li>• Membuat design website Sabea bersama tim untuk meningkatkan branding dan penjualan selama 10 hari, hasil yang didapatkan berupa peningkatan folowers sebesar 6,059% dengan menggunakan figma yang sudah berhasil di deploy “bisnisbarengsabea.com”</li>
+      </ul>
+    },
+    {
+      company: "Eduwork",
+      href: "https://eduwork.id/",
+      badges: [],
+      location: "Remote",
+      title: "UI/UX Designer (Remote)",
+      logoUrl: "/eduwork.png",
+      start: "Desember 2023",
+      end: "Maret 2024",
+      description:
+      <ul>
+        <li>• Merancang aplikasi manajemen keuangan dari data yang menunjukan bahwa 75% masalah yang dihadapi mahasiswa tingkat akhir menggunakan metode interview.</li>
+        <li>• Melakukan redesign aplikasi MyAgri berdasarkan review playstore dari 38 pengguna yang merasa kurang puas dengan tampilan hingga fitur disediakan.</li>
+        <li>• Melakukan riset untuk memahami kebutuhan pengguna dengan target maksimal 5 user untuk mendapatkan data yang lebih mendalam dengan melakukan interview dan survey.</li>
+        <li>• Membuat sketsa kasar lebih dari 10 screen untuk kerangka visual yang menggambarkan struktur hirarki elemen antarmuka dengan Figma dari hasil analisis.</li>
+        <li>• Menguji antarmuka dengan 3 user untuk mendapatkan perbaikan desain antarmuka berdasarkan pengalaman dengan menyiapkan skenario pengujian.</li>
+      </ul>
+    },
+    {
+      company: "Nuri",
+      href: "https://www.nuriglobal.com/",
+      badges: [],
+      location: "Remote",
+      title: "Project-Based Virtual Intern: UI/UX Designer",
+      logoUrl: "/Nuri.jpg",
+      start: "Oktober",
+      end: "Oktober 2023",
+      description:
+      <ul>
+        <li>• Mengumpulkan data dari 5 responden pelanggan COD dengan menggunakan metode in-dept interview.</li>
+        <li>• Melakukan pemetaan temuan masalah dari 5 responden dengan menggunakan problem statement.</li>
+        <li>• Melakukan brainstorming ide produk dengan membuat user flow, IA, Wireframe sebanyak 17 screen dan visual style.</li>
+        <li>• Melakukan perancangan Hi-Fi dan prototype sebanyak 17 screen berdasarkan hasil brainstorming.</li>
+        <li>• Membuat portofolio dari keseluruhan proses desain selama 3 hari yang dipublikasikan ke website behance.</li>
+      </ul>
+    },
+    {
+      company: "FundEx",
+      href: "https://fundex.id",
+      badges: [],
+      location: "Remote",
+      title: "Project-Based Virtual Intern: UI/UX Designer",
+      logoUrl: "/fundex.jpeg",
+      start: "Juli",
+      end: "Juli 2023",
+      description:
+      <ul>
+        <li>• Melakukan riset terhadap halaman situs FundEx selama 5 hari untuk memahami target dan kebutuhan dari perusahan.</li>
+        <li>• Melakukan elemen desain visual seperti warna, icon selama 1 hari untuk meningkatkan estetika halaman dengan tools Figma, Color Hunt, dan Tablericons.</li>
+        <li>• Mendefinisikan dan menyusun navigasi yang mudah dimengerti dan mencakup sekitar 30% dari keseluruhan proses untuk memandu pengguna melalui landing page.</li>
+        <li>• Membuat konten yang relevan, menarik, dan informatif, tersedia dan disajikan dalam 1 hari secara optimal berdasarkan brainstorming.</li>
+        <li>• Mengatur dan memformat teks, gambar dan elemen dalam waktu kurang dari 1 hari untuk meningkatkan daya tarik visual dengan pesan yang jelas.</li>
+      </ul>
+    },
+    {
+      company: "Binar Academy",
+      href: "https://www.binaracademy.com/",
+      badges: [],
+      location: "Remote",
+      title: "UI/UX Research and Design (Remote)",
+      logoUrl: "/binar.webp",
+      start: "Agustus 2022",
+      end: "Januari 2023",
+      description:
+      <ul>
+        <li>• Berkolaborasi untuk penelitian terkait masalah utama Indonesia dari data vaksinasi covid-19 '18 Agustus 2021' dengan diskusi.</li>
+        <li>• Melakukan perancangan desain produk berbasis mobile untuk pelanggan yang malas keluar rumah, hasil SUS score adalah 71 masuk dalam kategori baik dengan menggunakan metode Design Thinking.</li>
+        <li>• Melakukan perancangan ulang aplikasi MyUMM Student berdasarkan hasil review Playstore, hasil SUS score adalah 71, masuk dalam kategori 'good' dengan menggunakan analisis kompetitor.</li>
+        <li>• Berkolaborasi membuat aplikasi di bidang Agritech yaitu 'Ruang Tani' dan pengguna baru meningkat 20% dalam 1 bulan berdasarkan MVP Plan.</li>
+        <li>• Melakukan penelitian kesehatan mental untuk mahasiswa dan mahasiswi berdasarkan data 'Kesehatan Mental di Indonesia 2021' menggunakan problem statement.</li>
+        <li>• Melakukan manajemen proyek perangkat lunak Aplikasi Sistem Pakar Diagnosa Penyakit Ayam dengan anggaran 42.182.000 menggunakan metode waterfall.</li>
+      </ul>
+    },
+  ],
+  education: [
+    {
+      school: "Universitas Muhammadiyah Malang",
+      href: "-",
+      degree: "Bachelor Degree (S-1) - Informatics Engineering (3.87/4.00)",
+      logoUrl: "/umm.jpg",
+      start: "2019",
+      end: "2023",
+      description:
+      <ul>
+        <li>• Melakukan manajemen proyek perangkat lunak pada website "Sistem Pakar Diagnosa Penyakit Ayam" untuk peternak desa junrejo dengan menggunakan metode Waterfall.</li>
+        <li>• Melakukan analisis usability pada E-Learning (LMS) Universitas Muhammadiyah Malang dari sisi User Experience.</li>
+        <li>• Mengikuti kompetisi Gemastik pada tahun 2022 untuk para pejuang diabetes dengan merancang aplikasi pelacakan diabetes dengan kontrol kesehatan.</li>
+        <li>• Merancang aplikasi Sistem Informasi Desa Hitulama berbasis mobile dan website di Desa Hitulama untuk keperluan skripsi dengan metode Design Thinking.</li>
+        <li>• Merancang Sistem Informasi Manajemen Data Visual Desa Hitulama untuk membangun e-goverment dengan metode Design Thinking.</li>
+        <li>• Merancang Aplikasi Ayo Belajar untuk penyandang disabilitas dengan keterbatasan alat pendengaran</li>
+        <li>• Mempelajari bahasa pemrograman html, css, javascript, java, c++ dan dart.</li>
+      </ul>
+    },
+  ],
+  sertifikasi: [
+    {
+      pt: "Eduwork",
+      href: "https://eduwork.id/",
+      title: "Basic Principles UI/UX Designer",
+      logoUrl: "/eduwork.png",
+      start: "Desember 2023",
+      end: "Desember 2023",
+      description:
+      <ul>
+        <li>• Mempelajari prinsip-prinsip dasar dalam desain UI dan UX selama 1 minggu untuk mendesain antarmuka yang efektif dan mudah digunakan dengan Figma.</li>
+        <li>• Belajar memahami metode dan teknis analisis pengguna selama 2 hari untuk memahami kebutuhan dan perilaku pengguna dengan memilih metode yng tepat.</li>
+        <li>• Mempelajari teknik wireframe dan prototipe untuk dapat melakukan pengujian dengan skor di atas 70 kategori baik dengan Figma, Whmsical dan Maze.</li>
+        <li>• Belajar mengenai tools UI/UX selama 1 hari untuk membuat desain digital dengan mengeksplorasi sebanyak mungkin cara menggunakaan tools tersebut.</li>
+        <li>• Belajar mengenai kontras yang mengacu pada perbedaan elemen desain selama 1 jam terkait elemen yang berdekatan dan keseimbangan dengan color hunt dan Figma. </li>
+      </ul>
+    },
+    {
+      pt: "MySkill",
+      href: "https://myskill.id/",
+      title: "Figma for UI/UX Design & User Experience Design",
+      logoUrl: "/my skill.jpeg",
+      start: "September",
+      end: "November 2023",
+      description:
+      <ul>
+        <li>• Belajar mengenai desain sebuah produk selama 1 minggu untuk memberikan pengalaman pengguna yang baik dan mengurangi tingkat kebingungan/frustasi.</li>
+        <li>• Mempelajari tentang desain sebuah halaman mobile dan website selama 3 hari agar selalu konsisten dengan menggunakan elemen-elemen yang harus/wajib ada.</li>
+        <li>• Belajar mendalami aspek desain yang berfokus pada pengalaman pengguna selama 3 hari untuk menciptakan produk yang baik dengan menggunakan prinsip-prinsip UX.</li>
+        <li>• Belajar memahami pengguna selama 4 hari agar produk yang dibuat dapat menyelesaikan masalah pengguna dengan metode riset kualitatif dan kuantitatif.</li>
+        <li>• Belajar membuat dokumentasi desain terkait apa yang sudah dikerjakan untuk menyamakan persepsi semua pihak selama 1 hari dengan figma dan microsoft office.</li>
+      </ul>
+    },
+  ],
+  //mulain dari sini di edit aja i
+  projects: [
+    {
+      title: "Design Project Maintenance Management System Web Admin",
+      href: "",
+      dates: "03 Juli - 20 September 2024",
+      active: true,
+      description:
+      "Membuat design Maintenance Management Systems berbasis Web Admin untuk customer Tricentrum Fortuna",
+      technologies: [
+        "Figma",
+      ],
+      links: [
+        {
+          type: "Hasil Design",
+          href: "",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/Web tcf.png",
+    },
+    {
+      title: "Design Project Maintenance Management System Mobile App",
+      href: "",
+      dates: "24 Juni - 19 September 2024",
+      active: true,
+      description:
+      "Membuat design Maintenance Management Systems berbasis mobile app untuk customer Tricentrum Fortuna",
+      technologies: [
+        "Figma",
+      ],
+      links: [
+        {
+          type: "Hasil Design",
+          href: "",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/Mobile Tcf.png",
+    },
+    {
+      title: "Design Project Maintenance Checklist Mobile App",
+      href: "",
+      dates: "12 - 17 September 2024",
+      active: true,
+      description:
+      "Membuat design Maintenance Management Systems berbasis mobile app customer SHARP",
+      technologies: [
+        "Figma",
+      ],
+      links: [
+        {
+          type: "Hasil Design",
+          href: "https://drive.google.com/file/d/1i_EARIR5Ais5ZOFNTpxpI28Yvqr1RDkn/view?usp=sharing",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/Sharp Mobile.png",
+    },
+    {
+      title: "Design Concept Web Admin QC System",
+      href: "",
+      dates: "11 September 2024",
+      active: true,
+      description:
+      "Membuat design concept Quality Control System berbasis web admin untuk customer pt shoftren indonesia",
+      technologies: [
+        "Figma",
+      ],
+      links: [
+        {
+          type: "Hasil Design",
+          href: "https://drive.google.com/file/d/1mWxYawDd1SFyltuThkT4KaXL9MO7Hxic/view?usp=sharing",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/QC System.png",
+    },
+  ],
+} as const;
